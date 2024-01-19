b10b2cc1665c79287cb4000473f5a2949ebfb5e3
kareem172 <kareemkhalaf1722@gmail.com>
2024-01-19 15:16:12 +0200

    add discount filtering to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -4,28 +4,42 @@ import { useCabins } from "./useCabins";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, data: cabins } = useCabins();
 
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("discount");
+  let filteredCabins = cabins;
+  switch (filter) {
+    case "noDiscount":
+      filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+      break;
+    case "withDiscount":
+      filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+      break;
+    default:
+      filteredCabins = cabins;
+  }
+
   if (isLoading) return <Spinner />;
   return (
     <Menus>
-
-    <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
-      <Table.Header>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
-      <Table.Body
-        data={cabins}
-        render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
-      />
-    </Table>
+      <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={filteredCabins}
+          render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
     </Menus>
   );
 }
--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,4 +1,6 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
+import propTypes from "prop-types";
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +35,36 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const selectedValue = searchParams.get(filterField) || options[0].value;
+  const handleClick = (value) => {
+    searchParams.set(filterField, value);
+    setSearchParams(searchParams, { replace: true });
+  };
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={selectedValue === option.value}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+Filter.propTypes = {
+  filterField: propTypes.string.isRequired,
+  options: propTypes.arrayOf(
+    propTypes.shape({
+      value: propTypes.string.isRequired,
+      label: propTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+export default Filter;
