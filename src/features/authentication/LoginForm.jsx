f8b7293e8f43d4d4e7bad3069b74f71c1b26954f
kareem172 <kareemkhalaf1722@gmail.com>
2024-01-25 19:22:57 +0200

    Update initial values for email and password fields in LoginForm

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("kareem@example.com");
-  const [password, setPassword] = useState("123456");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
